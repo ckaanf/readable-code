708d99d6bf62e2a1b4baf6f9faf50d1dd2ef0247
ckaanf <skywlstn777@gmail.com>
2024-10-09 15:45:57 +0900

    refactoring : Main() 변경

--- src/main/java/cleancode/studycafe/tobe/StudyCafeApplication.java
+++ src/main/java/cleancode/studycafe/tobe/StudyCafeApplication.java
@@ -1,10 +1,15 @@
 package cleancode.studycafe.tobe;
 
+import cleancode.studycafe.tobe.model.StudyCafeAdmin;
+import cleancode.studycafe.tobe.model.machine.StudyCafePassMachine;
+import cleancode.studycafe.tobe.model.machine.ThisStudyCafePassMachine;
+
 public class StudyCafeApplication {
 
     public static void main(String[] args) {
-        ThisStudyCafePassMachine thisStudyCafePassMachine = new ThisStudyCafePassMachine();
-        thisStudyCafePassMachine.service();
+        StudyCafeAdmin admin = new StudyCafeAdmin();
+        StudyCafePassMachine machine = new ThisStudyCafePassMachine();
+        machine.service(admin);
     }
 
 }
