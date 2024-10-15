6bf84cc0381cfb4e84d131ff17c17674997c325b
Kim Jin Soo <ckaanf@accurasoft.co.kr>
2024-10-15 10:44:24 +0900

    mission : day-12 - 사물함 이용권 테스트 코드 개선

--- src/test/java/cleancode/studycafe/tobe/model/pass/locker/StudyCafeLockerPassesTest.java
+++ src/test/java/cleancode/studycafe/tobe/model/pass/locker/StudyCafeLockerPassesTest.java
@@ -9,15 +9,9 @@ import org.junit.jupiter.api.Test;
 
 import cleancode.studycafe.tobe.model.pass.StudyCafePassType;
 import cleancode.studycafe.tobe.model.pass.StudyCafeSeatPass;
-import cleancode.studycafe.tobe.model.stub.StudyCafeLockerPassList;
+import cleancode.studycafe.tobe.model.stub.StudyCafeLockerPassStubList;
 
 class StudyCafeLockerPassesTest {
-	StudyCafeLockerPassList studyCafeLockerPassList = new StudyCafeLockerPassList();
-
-	@DisplayName("")
-	@Test
-	void of() {
-	}
 
 	@DisplayName("사물함 이용권에서 좌석 이용권과 기간이 같은 이용권을 찾는다.")
 	@Test
@@ -25,13 +19,19 @@ class StudyCafeLockerPassesTest {
 		// given
 		StudyCafeSeatPass seatPass = StudyCafeSeatPass.of(StudyCafePassType.FIXED, 4, 250000, 0.1);
 		StudyCafeLockerPasses studyCafeLockerPasses = StudyCafeLockerPasses.of(
-			studyCafeLockerPassList.getLockerPassList());
+			StudyCafeLockerPassStubList.getLockerPassList());
 
 		// when
 		Optional<StudyCafeLockerPass> studyCafeLockerPass = studyCafeLockerPasses.findLockerPassBy(seatPass);
 
 		// then
+		// Optional 객체 있는 지 먼저 검증하고 그 다음에 있으면 안에 값 검증 체이닝으로
 		assertThat(studyCafeLockerPass.get().getDuration()).isEqualTo(seatPass.getDuration());
+
+		assertThat(studyCafeLockerPass).isNotEmpty()
+			.get()
+			.extracting(StudyCafeLockerPass::getDuration)
+			.isEqualTo(seatPass.getDuration());
 	}
 
 	@DisplayName("좌석 이용권과 동일한 기간을 가진 사물함 이용권이 없으면 빈 Optional 객체를 반환한다.")
@@ -40,7 +40,7 @@ class StudyCafeLockerPassesTest {
 		// given
 		StudyCafeSeatPass seatPass = StudyCafeSeatPass.of(StudyCafePassType.WEEKLY, 4, 250000, 0.1);
 		StudyCafeLockerPasses studyCafeLockerPasses = StudyCafeLockerPasses.of(
-			studyCafeLockerPassList.getLockerPassList());
+			StudyCafeLockerPassStubList.getLockerPassList());
 
 		// when
 		Optional<StudyCafeLockerPass> studyCafeLockerPass = studyCafeLockerPasses.findLockerPassBy(seatPass);
